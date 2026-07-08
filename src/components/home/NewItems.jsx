8b8152804b1e3c611ab6a58c3b60c3dff5b385f0
bt02123 <bryson.a.terry@gmail.com>
2026-07-07 21:34:37 -0400

    Top Sellers Section complete

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -133,7 +133,7 @@ const NewItems = () => {
                         to={`/author/${item.authorId}`}
                         data-bs-toggle="tooltip"
                         data-bs-placement="top"
-                        title="Creator: Monica Lucas"
+                        title={item.authorId}
                       >
                         <img className="lazy" src={item.authorImage} alt="" />
                         <i className="fa fa-check"></i>
