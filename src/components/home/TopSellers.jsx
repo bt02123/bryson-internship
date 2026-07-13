cf4ff23ee0f16dec1706f2b26323f9c88ee625d2
bt02123 <bryson.a.terry@gmail.com>
2026-07-13 16:06:22 -0400

    vercel log error fixes

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import AuthorItems from "../author/AuthorItems";
 
 const TopSellers = () => {
   const [topSellers, setTopSellers] = useState([]);
